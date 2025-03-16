d4114eff3c455c6286065125848ed41fa5ca416f
nikitaSB01 <boss.shvabauer@bk.ru>
2025-03-16 16:32:09 +0500

    доделал компоненты

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useJsonFetch } from "../hooks/useJsonFetch";
 
-export const DataComponent = () => {
+export const ErrorComponent = () => {
   const [data, loading, error] = useJsonFetch<{ status: string }>(
-    "http://localhost:7070/data"
+    "http://localhost:7070/error"
   );
 
   return (
     <div style={{ marginBottom: "1rem" }}>
-      <h2>/data</h2>
+      <h2>/error</h2>
       {loading && <p>Загрузка...</p>}
       {error && <p style={{ color: "red" }}>Ошибка: {error}</p>}
-      {data && <p>Ответ: {data.status}</p>}
+      {data && <p>Ответ: {JSON.stringify(data)}</p>}
     </div>
   );
 };
--- src/components/LoadingComponent.tsx
+++ src/components/LoadingComponent.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useJsonFetch } from "../hooks/useJsonFetch";
 
-export const DataComponent = () => {
+export const LoadingComponent = () => {
   const [data, loading, error] = useJsonFetch<{ status: string }>(
-    "http://localhost:7070/data"
+    "http://localhost:7070/loading"
   );
 
   return (
     <div style={{ marginBottom: "1rem" }}>
-      <h2>/data</h2>
-      {loading && <p>Загрузка...</p>}
+      <h2>/loading</h2>
+      {loading && <p>Загрузка (ожидай 5 секунд)...</p>}
       {error && <p style={{ color: "red" }}>Ошибка: {error}</p>}
       {data && <p>Ответ: {data.status}</p>}
     </div>
